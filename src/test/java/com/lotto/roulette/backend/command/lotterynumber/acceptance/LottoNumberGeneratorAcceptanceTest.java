17087275182412e7b7a40b96c145b08c20d8041d
CMHyuk <alsgur9710@naver.com>
2024-02-18 21:15:50 +0900

    test: 로또 번호 생성 인수 테스트

---
+++ src/test/java/com/lotto/roulette/backend/command/lotterynumber/acceptance/LottoNumberGeneratorAcceptanceTest.java
@@ -0,0 +1,28 @@
+package com.lotto.roulette.backend.command.lotterynumber.acceptance;
+
+import com.lotto.roulette.backend.support.enviroment.AcceptanceTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoNumberGeneratorAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    void 로또_번호를_생성한다() {
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .post("/lotto-numbers/custom")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
