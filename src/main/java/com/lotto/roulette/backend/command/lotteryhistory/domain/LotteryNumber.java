24e7686d71081f194efca024e6185cbd59d08778
CMHyuk <alsgur9710@naver.com>
2024-02-18 17:25:54 +0900

    feat: 로또 번호 검증

--- src/main/java/com/lotto/roulette/backend/command/lotteryhistory/domain/LotteryNumber.java
+++ src/main/java/com/lotto/roulette/backend/command/lotteryhistory/domain/LotteryNumber.java
@@ -1,5 +1,7 @@
 package com.lotto.roulette.backend.command.lotteryhistory.domain;
 
+import com.lotto.roulette.backend.command.lotteryhistory.exception.LotteryHistoryException;
+import com.lotto.roulette.backend.common.exception.BusinessException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
@@ -10,6 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class LotteryNumber {
+
+    private static final int FIRST_NUMBER = 1;
+    private static final int LAST_NUMBER = 45;
+
 
     @Column(nullable = false)
     private int firstLottoNumber;
@@ -37,11 +43,23 @@ public class LotteryNumber {
 
     private LotteryNumber(int firstLottoNumber, int secondLottoNumber, int thirdLottoNumber,
                           int fourthLottoNumber, int fifthLottoNumber, int sixthLottoNumber) {
+        validateLottoNumberRange(firstLottoNumber);
+        validateLottoNumberRange(secondLottoNumber);
+        validateLottoNumberRange(thirdLottoNumber);
+        validateLottoNumberRange(fourthLottoNumber);
+        validateLottoNumberRange(fifthLottoNumber);
+        validateLottoNumberRange(sixthLottoNumber);
         this.firstLottoNumber = firstLottoNumber;
         this.secondLottoNumber = secondLottoNumber;
         this.thirdLottoNumber = thirdLottoNumber;
         this.fourthLottoNumber = fourthLottoNumber;
         this.fifthLottoNumber = fifthLottoNumber;
         this.sixthLottoNumber = sixthLottoNumber;
+    }
+
+    private static void validateLottoNumberRange(int number) {
+        if (number < FIRST_NUMBER || number > LAST_NUMBER) {
+            throw BusinessException.from(LotteryHistoryException.INVALID_LOTTERY_NUMBER);
+        }
     }
 }
