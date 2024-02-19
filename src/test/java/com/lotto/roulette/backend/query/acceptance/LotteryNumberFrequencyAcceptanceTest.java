d1ed4ae39e7f219cb2b5b2ab993fb39535d0ba24
CMHyuk <alsgur9710@naver.com>
2024-02-19 16:09:02 +0900

    test: 로또 번호 빈도 조회 인수 테스트

---
+++ src/test/java/com/lotto/roulette/backend/query/acceptance/LotteryNumberFrequencyAcceptanceTest.java
@@ -0,0 +1,34 @@
+package com.lotto.roulette.backend.query.acceptance;
+
+import com.lotto.roulette.backend.support.enviroment.AcceptanceTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Sql("/fixture/lottery-number-frequency-fixture.sql")
+public class LotteryNumberFrequencyAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    void 로또_번호들의_빈도수를_조회한다() {
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .get("/lottery-number-frequency")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getList(".")).hasSize(45)
+        );
+    }
+}
