4b8b31a7f5d65c9f0191ce8001d171ddc5f7ea9f
CMHyuk <alsgur9710@naver.com>
2024-02-19 13:49:47 +0900

    test: 로또 당첨 번호 빈도 수 조회 테스트

---
+++ src/test/java/com/lotto/roulette/backend/query/service/LotteryNumberFrequencyQueryServiceTest.java
@@ -0,0 +1,29 @@
+package com.lotto.roulette.backend.query.service;
+
+import com.lotto.roulette.backend.query.dto.LotteryNumberFrequencyResponse;
+import com.lotto.roulette.backend.support.enviroment.ServiceTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Sql("/fixture/lottery-number-frequency-fixture.sql")
+class LotteryNumberFrequencyQueryServiceTest extends ServiceTest {
+
+    @Autowired
+    private LotteryNumberFrequencyQueryService lotteryNumberFrequencyQueryService;
+
+    @Test
+    void 로또_번호들의_빈도수를_조회한다() {
+        // when
+        List<LotteryNumberFrequencyResponse> results = lotteryNumberFrequencyQueryService.findAll();
+
+        // then
+        assertThat(results.size()).isEqualTo(45);
+        assertThat(results.get(37).lottoNumber()).isEqualTo(38);
+        assertThat(results.get(37).frequency()).isEqualTo(44);
+    }
+}
