789f15e2f081f324ecdc52c3632c76e37811c432
CMHyuk <alsgur9710@naver.com>
2024-03-05 14:40:15 +0900

    feat: 애플리케이션 실행 시 로또 당첨 이력 데이터 삽입

--- src/main/java/com/lotto/roulette/RouletteApplication.java
+++ src/main/java/com/lotto/roulette/RouletteApplication.java
@@ -1,19 +1,35 @@
 package com.lotto.roulette;
 
+import com.lotto.roulette.backend.command.lotteryhistory.application.LotteryHistoryService;
+import com.lotto.roulette.backend.command.lotteryhistory.infrastructure.LotteryHistoryUtils;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @EnableCaching
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
+@RequiredArgsConstructor
 public class RouletteApplication {
+
+    private final LotteryHistoryService lotteryHistoryService;
 
     public static void main(String[] args) {
         SpringApplication.run(RouletteApplication.class, args);
     }
 
+    @PostConstruct
+    void insertLotteryHistories() {
+        File excel = new File("src/main/resources/lotto.xlsx");
+        MultipartFile lotteryHistoryExcel = LotteryHistoryUtils.convertFileToMultipartFile(excel);
+        lotteryHistoryService.saveAll(lotteryHistoryExcel);
+    }
 }
--- src/main/java/com/lotto/roulette/backend/command/lotteryhistory/infrastructure/LotteryHistoryUtils.java
+++ src/main/java/com/lotto/roulette/backend/command/lotteryhistory/infrastructure/LotteryHistoryUtils.java
@@ -12,14 +12,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import static java.util.stream.IntStream.range;
 
 @Component
 @RequiredArgsConstructor
-public class LotteryHistoryParser {
+public class LotteryHistoryUtils {
 
     private static final DataFormatter formatter = new DataFormatter();
     private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -44,5 +49,53 @@ public class LotteryHistoryParser {
         } catch (IOException e) {
             throw BusinessException.from(new InternalServerErrorCode(e.getMessage()));
         }
+    }
+
+    public static MultipartFile convertFileToMultipartFile(File lotteryHistoryExcel) {
+        return new MultipartFile() {
+            @Override
+            public String getName() {
+                return lotteryHistoryExcel.getName();
+            }
+
+            @Override
+            public String getOriginalFilename() {
+                return lotteryHistoryExcel.getName();
+            }
+
+            @Override
+            public String getContentType() {
+                return "xlsx";
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public long getSize() {
+                return lotteryHistoryExcel.length();
+            }
+
+            @Override
+            public byte[] getBytes() throws IOException {
+                FileInputStream fileInputStream = new FileInputStream(lotteryHistoryExcel);
+                byte[] bytes = new byte[(int) lotteryHistoryExcel.length()];
+                fileInputStream.read(bytes);
+                fileInputStream.close();
+                return bytes;
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return new FileInputStream(lotteryHistoryExcel);
+            }
+
+            @Override
+            public void transferTo(File dest) throws IOException, IllegalStateException {
+                Files.copy(lotteryHistoryExcel.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+        };
     }
 }
